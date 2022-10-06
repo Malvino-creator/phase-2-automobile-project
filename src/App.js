44ce94c514dc02b75b5df5fa5096ca3b08e123d4
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-06 15:32:59 +0300

    feat(App): Implement the routes of the Navbar Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import Navbar from './components/NavBar';
+import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Menu from './components/Menu';
 import About from './components/About';
 import Contact from './components/Contact';
 import './App.css';
-import {BrowserRoute, Routes, Route} from "react-router-dom";
+import {Router, Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
+  return (                                                                                                                                                                                                                                                                                                                                                                                                
     <div className="App">
+      <Router>
+        <NavBar />
+      <Routes>
+      <Route path="/" element={Home} />
+      <Route path="/menu" element={Menu} />
+      <Route path="/about" element={About} />
+      <Route path="/contact" element={Contact} />
+      </Routes>
+      </Router>
       
     </div>
   );
