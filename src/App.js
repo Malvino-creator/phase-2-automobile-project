b5129a10fbb3de95c4143ed3505fe10bc3c57f74
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-06 17:01:40 +0300

    feat(Assets): Create assets folder and transfer images into it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import Footer from './components/Footer';
 import Home from './components/Home';
 import Menu from './components/Menu';
 import About from './components/About';
