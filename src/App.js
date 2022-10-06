e43747e60fce2b9518dd45d09d27e69095149211
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-06 14:09:26 +0300

    Import the Browser's routes to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Menu from './components/Menu';
+import About from './components/About';
+import Contact from './components/Contact';
 import './App.css';
+import {BrowserRoute, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
