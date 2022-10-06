459d9fc88ef17df45b749455bcef51d64004a31e
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-06 11:44:27 +0300

    feat(footer): Optimises the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,2 +1,16 @@
 import React from "react";
-import InstagramIcon from "@material-ui/icons/Instagram"
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import FaceBookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import "../styles/Footer.css";
+
+function Footer() {
+    return(
+        <div className="footer">
+            <div className="socialmedia">
+                <Instagram /> <TwitterIcon /> <FaceBookIcon /> <LinkedInIcon />
+            </div>
+        </div>
+    )
+}
